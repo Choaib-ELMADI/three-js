3e824fe504201f4dcb92af0a520d1b61fd8a1d5c
Choaib Elmadi <111356633+Choaib-ELMADI@users.noreply.github.com>
2023-04-08 15:21:25 +0000

    Add files via upload

---
+++ src/glsl/fragmentShader.glsl
@@ -0,0 +1,10 @@
+void main() {
+    // Instructions
+
+                         R    G    B  Opacity
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+
+
+The role of a fragment shader is to calorize 
+vertices in the mesh they form
---
+++ src/glsl/vertexShader.glsl
@@ -0,0 +1,12 @@
+/*
+void main() {
+    //* Instructions
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
+*/
+
+/*
+    The vertex shader takes care of positioning 
+    every vertex composing a mesh in the scene
+*/
